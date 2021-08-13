ef3ebc2da3ff4cf72002a5b20468f3983f9a04a4
Julio Dutra <jjcdutra@gmail.com>
2021-08-13 20:02:22 -0300

    Retornando um customer no get

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,15 +1,16 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("customer")
+@RequestMapping("customers")
 class CustomerController {
 
     @GetMapping
-    fun helloWorld(): String {
-        return "Hello World"
+    fun helloWorld(): CustomerModel {
+        return CustomerModel("1", "Julio", "email@email")
     }
 }
