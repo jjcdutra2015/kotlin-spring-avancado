7b79ce03bb1ea6a79b304fcf9a2b13e17c5bc5b1
Julio Dutra <jjcdutra@gmail.com>
2021-08-18 21:35:13 -0300

    Alterando o retorno de BookModel e CustomerModel

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -2,8 +2,9 @@ package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostBookRequest
 import com.mercadolivro.controller.request.PutBookRequest
+import com.mercadolivro.controller.response.BookResponse
 import com.mercadolivro.extension.toBookModel
-import com.mercadolivro.model.BookModel
+import com.mercadolivro.extension.toResponse
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
@@ -24,17 +25,17 @@ class BookController(
     }
 
     @GetMapping
-    fun findAll(): List<BookModel> {
-        return bookService.findAll()
+    fun findAll(): List<BookResponse> {
+        return bookService.findAll().map { it.toResponse() }
     }
 
     @GetMapping("/actives")
-    fun findActive(): List<BookModel> =
-        bookService.findActive()
+    fun findActive(): List<BookResponse> =
+        bookService.findActive().map { it.toResponse() }
 
     @GetMapping("/{id}")
-    fun finById(@PathVariable id: Int): BookModel {
-        return bookService.findById(id)
+    fun finById(@PathVariable id: Int): BookResponse {
+        return bookService.findById(id).toResponse()
     }
 
     @DeleteMapping("/{id}")
