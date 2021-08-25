e697ec0b6fcaf4d5fdc163f79d0bbac7f7c3194d
Julio Dutra <jjcdutra@gmail.com>
2021-08-24 21:05:48 -0300

    Criando uma custim validation

--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -42,4 +42,8 @@ class CustomerService(
         customer.status = CustomerStatus.INATIVO
         customerRepository.save(customer)
     }
+
+    fun emailAvailable(value: String): Boolean {
+        return !customerRepository.existsByEmail(value);
+    }
 }
