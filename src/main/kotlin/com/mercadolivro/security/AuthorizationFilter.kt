698cf064f264b52fc5bc79b55aaf06e108e58bb8
Julio Dutra <jjcdutra@gmail.com>
2021-09-11 10:53:30 -0300

    Implementando o filter de autorização

---
+++ src/main/kotlin/com/mercadolivro/security/AuthorizationFilter.kt
@@ -0,0 +1,37 @@
+package com.mercadolivro.security
+
+import com.mercadolivro.exception.AuthenticationException
+import com.mercadolivro.service.UserDetailsCustomService
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class AuthorizationFilter(
+    authenticationManager: AuthenticationManager,
+    private val userDetailsCustomService: UserDetailsCustomService,
+    private val jwtUtil: JwtUtil
+) : BasicAuthenticationFilter(authenticationManager) {
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+        val header = request.getHeader("Authorization")
+        if (header != null && header.startsWith("Bearer ")) {
+            val authentication = getAuthentication(header.split(" ")[1])
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+
+        chain.doFilter(request, response)
+    }
+
+    private fun getAuthentication(token: String): UsernamePasswordAuthenticationToken {
+        if (!jwtUtil.isValidToken(token)) {
+            throw AuthenticationException("Token inválido", "999")
+        }
+        val id = jwtUtil.getSubject(token)
+        val customer = userDetailsCustomService.loadUserByUsername(id)
+        return UsernamePasswordAuthenticationToken(id, null, customer.authorities)
+    }
+}
